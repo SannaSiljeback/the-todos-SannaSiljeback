bf2605305f8545ac6be2aa22eadbfc03f813517a
Sanna Siljebäck <sannasiljeback@gmail.com>
2024-01-23 13:25:55 +0100

    done with remove button

--- src/components/BucketListApp.tsx
+++ src/components/BucketListApp.tsx
@@ -1,4 +1,4 @@
-// grunden för vår app, där funktionaliteten ligger, som en förlängning av App
+// grunden för vår app, där funktionaliteten ligger, som en förlängning av App, är vår root komponent
 
 import { useState } from "react";
 import { ShowBucketList } from "./ShowBucketList";
@@ -12,16 +12,15 @@ export const BucketListApp = () => {
     new BucketList(4, "Äta mexikansk tacos", "Mexico", false),
     new BucketList(5, "Åka tåg mellan Kandy och Ella", "Sri Lanka", true),
   ]);
-  
-// funktion för att ta bort ett helt objekt ur listan
-// const removeBucketList = (id: number) => {
-//     setBucketLists(bucketLists.filter((bucketList) => bucketList.id !== id));
-// };
 
+  // funktion för att ta bort ett helt objekt ur listan
+  const removeBucketList = (id: number) => {
+      setBucketLists(bucketLists.filter((bucketList) => bucketList.id !== id));
+  };
 
   return (
     <>
-      <ShowBucketList bucketList={bucketLists} />
+      <ShowBucketList bucketList={bucketLists} removeBucketList={removeBucketList}/>
     </>
   );
 };
--- src/components/ShowBucketList.tsx
+++ src/components/ShowBucketList.tsx
@@ -1,12 +1,22 @@
 // som en mall som visar hur vår lista/objekt ska visas upp
 
+// import { useState } from "react";
 import { BucketList } from "../models/BucketList";
 
 interface IShowBucketListProps {
   bucketList: BucketList[];
+  removeBucketList: (id: number) => void;
 }
 
 export const ShowBucketList = (props: IShowBucketListProps) => {
+  // const [bucketLists, setBucketLists] = useState<BucketList[]>([
+  //   new BucketList(1, "Hoppa fallskärm", "Australien", true),
+  //   new BucketList(2, "Besöka Machu Picchu", "Peru", false),
+  //   new BucketList(3, "Vandra El Camino Frances", "Spanien", false),
+  //   new BucketList(4, "Äta mexikansk tacos", "Mexico", false),
+  //   new BucketList(5, "Åka tåg mellan Kandy och Ella", "Sri Lanka", true),
+  // ]);
+
   return (
     <>
       <h1>My Awesome Bucket List</h1>
@@ -14,9 +24,11 @@ export const ShowBucketList = (props: IShowBucketListProps) => {
       <button>Lägg till</button>
       {props.bucketList.map((bucketList) => (
         <div key={bucketList.id}>
-         <input type="checkbox"/>
-         <span> {bucketList.name}, {bucketList.place} </span>
-         <button >Ta bort</button> 
+          <input type="checkbox" />
+          <span>
+            {bucketList.name}, {bucketList.place}
+          </span>
+          <button onClick={() => props.removeBucketList(bucketList.id)}>Ta bort</button>
         </div>
       ))}
     </>
