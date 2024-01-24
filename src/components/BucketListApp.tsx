84fb21366831ba64273b71e95ec6ee617498bbe4
Sanna Siljebäck <sannasiljeback@gmail.com>
2024-01-24 10:21:56 +0100

    almost done with local storage

--- src/components/BucketListApp.tsx
+++ src/components/BucketListApp.tsx
@@ -6,51 +6,53 @@ import { BucketList } from "../models/BucketList";
 import { AddBucketList } from "./AddBucketList";
 
 export const BucketListApp = () => {
-  const [bucketLists, setBucketLists] = useState<BucketList[]>([
-    new BucketList(1, "Hoppa fallskärm", "Australien", true),
-    new BucketList(2, "Besöka Machu Picchu", "Peru", false),
-    new BucketList(3, "Vandra El Camino Frances", "Spanien", false),
-    new BucketList(4, "Äta mexikansk tacos", "Mexico", false),
-    new BucketList(5, "Åka tåg mellan Kandy och Ella", "Sri Lanka", true),
-  ]);
+  // hämta information från localstorage, syntax:
+  // localStorage.getItem(key:string): string;
+  // const listStorage = localStorage.getItem("bucketLists");
+  // const listStorageAsObject = JSON.parse(listStorage);
+  const hardCodedValues = [
+    new BucketList(0, "Hoppa fallskärm", "Australien", true),
+    new BucketList(1, "Besöka Machu Picchu", "Peru", false),
+    new BucketList(2, "Vandra El Camino Frances", "Spanien", false),
+    new BucketList(3, "Äta mexikansk tacos", "Mexico", false),
+    new BucketList(4, "Åka tåg mellan Kandy och Ella", "Sri Lanka", true),
+  ];
+
+  // går göra på en rad: sätts i use statet då
+  //state för local storage:
+  // const [bucketListsStorage, setBucketListsStorage] = useState(JSON.parse(localStorage.getItem("bucketLists") || "[]"));
+  // skippa [] och ersätta medsin hårdkodade lista?
+
+  const [bucketLists, setBucketLists] = useState<BucketList[]>(
+    JSON.parse(localStorage.getItem("listStorage") || JSON.stringify(hardCodedValues)));
+
 
   // funktion för att ta bort ett helt objekt ur listan
   const removeBucketListFunction = (id: number) => {
-      setBucketLists(bucketLists.filter((bucketList) => bucketList.id !== id));
-      
+    setBucketLists(bucketLists.filter((bucketList) => bucketList.id !== id));
   };
 
   // funktion för att lägga till ett helt objekt i listan
   const addBucketListFunction = (newBucketListName: string, newBucketListPlace: string) => {
-    setBucketLists([...bucketLists, new BucketList(bucketLists.length +1, newBucketListName, newBucketListPlace, false)]);
+    setBucketLists([...bucketLists, new BucketList(bucketLists.length + 1, newBucketListName, newBucketListPlace, false)]);
 
-    //state för local storage:
-    // const [bucketListsStorage, setBucketListsStorage] = useState(JSON.parse(localStorage.getItem("bucketLists") || "[]"));
+    console.log(bucketLists);
 
-
-    //localStorage här? vill ju spara värdet från våra inputs? spara ner i localstorage, syntax:
-    localStorage.setItem("listStorage", JSON.stringify(bucketLists));
-    // vill vi spara ner input.value här ist för bucketLists?
-
-    // hämta information från localstorage, syntax:
-    // localStorage.getItem(key:string): string;
-    // const listStorage = localStorage.getItem("bucketLists");
-    // const listStorageAsObject = JSON.parse(listStorage);
-
-    // går göra på en rad: sätts i use statet då
-    // JSON.parse(localStorage.getItem("bucketLists") || "[]")
-    // skippa [] och ersätta medsin hårdkodade lista?
-
+  
+    //localStorage här? spara ner i localstorage, syntax:
+    localStorage.setItem("listStorage",JSON.stringify([...bucketLists, new BucketList(bucketLists.length + 1, newBucketListName, newBucketListPlace, false)]));
+   
   };
 
   return (
     <>
       <h1>My Awesome Bucket List</h1>
 
-      <AddBucketList iAddBucketList={addBucketListFunction}/>
-      <ShowBucketList iBucketList={bucketLists} iRemoveBucketList={removeBucketListFunction}/>
-
-  
+      <AddBucketList iAddBucketList={addBucketListFunction} />
+      <ShowBucketList
+        iBucketList={bucketLists}
+        iRemoveBucketList={removeBucketListFunction}
+      />
     </>
   );
 };
